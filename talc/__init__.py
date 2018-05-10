5e8395c918429268012596dac69ae01a607e06d7
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2018-05-10 11:46:24 +0200

    Export implemented distances for error measurement

--- talc/__init__.py
+++ talc/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
 from .talc import get_metadata, get_audio_list, get_audio_filepath, get_time_annotations
-from .evaluation import stats, error
+from .evaluation import stats, error, dist_both, dist_start
